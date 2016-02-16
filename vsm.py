b8dfc10a79067832d594de1472cb2b6619a9a64a
Chua Jie Sheng <chuajiesheng@gmail.com>
2016-02-17 00:23:43 +0800

    Implement tf idf function

--- vsm.py
+++ vsm.py
@@ -1,5 +1,7 @@
 import math
+import collections
 import numpy as np
+import scipy.sparse as sps
 
 
 TOTAL_NUMBER_OF_DOCUMENTS = 2500
@@ -38,6 +40,16 @@ def valid_word(word):
     return len(word) > 0 and word.isalnum()
 
 
+def get_number_of_occurrences_in_doc(documents):
+    word_occurrences = dict()
+    for words in documents:
+        unique_words = set(words)
+        map(lambda w: add_one(word_occurrences, w), unique_words)
+
+    # sort
+    return collections.OrderedDict(sorted(word_occurrences.items()))
+
+
 def add_one(dictionary, word):
     if word in dictionary:
         dictionary[word] += 1
@@ -45,9 +57,9 @@ def add_one(dictionary, word):
         dictionary[word] = 1
 
 
-def get_idf(total_documents, occurrences):
+def get_idf(total_documents, word_occurrences):
     idf_dict = dict()
-    for key, value in occurrences.iteritems():
+    for key, value in word_occurrences.iteritems():
         inverse_freq = float(total_documents) / float(1 + value)
         idf_dict[key] = math.log(inverse_freq)
 
@@ -56,22 +68,25 @@ def get_idf(total_documents, occurrences):
 
 
 def get_empty_tf_matrix(rows, words):
-    tf_formats = ['longdouble'] * len(words)
-    return np.zeros((rows, len(words)), dtype={'names': words, 'formats': tf_formats})
+    return sps.lil_matrix((rows, len(words)), dtype=np.longdouble)
+
+
+def get_tf(total_documents, documents, words):
+    matrix = get_empty_tf_matrix(total_documents, words)
+    for row, doc in enumerate(documents):
+        for word in doc:
+            col = words.index(word)
+            matrix[row, col] += 1
+    return matrix
 
 
 if __name__ == '__main__':
     docs = read_documents()
-
-    occurrences = dict()
-    for doc in docs:
-        map(lambda w: add_one(occurrences, w), doc)
-
+    occurrences = get_number_of_occurrences_in_doc(docs)
     idf = get_idf(TOTAL_NUMBER_OF_DOCUMENTS, occurrences)
-    tf = get_empty_tf_matrix(TOTAL_NUMBER_OF_DOCUMENTS, occurrences.keys())
 
-    print tf.shape
-    print idf.shape
+    indexed_words = occurrences.keys()
+    tf = get_tf(TOTAL_NUMBER_OF_DOCUMENTS, docs, indexed_words)
 
-    tf_idf = np.dot(tf.astype(np.longdouble), idf)
-    print tf_idf.shape
+    tf_idf = np.dot(tf.toarray(), idf)
+    print tf_idf
